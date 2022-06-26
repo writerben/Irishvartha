8e0bf1760d58070769c6cc1a5320e9abb7f5efae
writerben <64561235+writerben@users.noreply.github.com>
2022-06-26 17:59:34 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="home-page" class="page-wrapper home-page">
-    //<site-hero :title="title" :subtitle="subtitle" :image="featureImage">
+    <site-hero :title="title" :subtitle="subtitle" :image="featureImage">
       
-    //</site-hero>
+    </site-hero>
     <main-section theme="one-column">
       <template v-slot:default>
         <!-- All Posts -->
